50eb2728a85ea816b6af5ebefd74eae8876c56ec
Thomas Di Martino <thomas.di-martino@hotmail.com>
2020-11-07 16:31:33 +0100

    Adding setup.py and travis support

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Access Google's multi-petabytes of SAR Imagery data from your python code with *
 This enables quick data analysis of GRD images to get better insights of the temporal dimension in SAR data without having to bother with essential but potentially time-consuming steps such as coregistration or calibration. 
 
 Compatible with python 3.
-
+[![Documentation Status](https://readthedocs.org/projects/gee-sar-fetcher/badge/?version=latest)](https://gee-sar-fetcher.readthedocs.io/en/latest/?badge=latest)
+[![PyPI version](https://badge.fury.io/py/geesarfetcher.svg)](https://badge.fury.io/py/geesarfetcher)
 ## Usage
 ### Python Import
 The main function of this library is the ``fetcher`` function:
@@ -20,7 +21,7 @@ fetch(
     start_date = date.today()-timedelta(days=15),
     end_date = date.today(),
     ascending=False
-) # returns a dictionnary with access to the data through the 'stack' keyword and to its timestamps through the 'timestamps' keywordc
+) # returns a dictionnary with access to the data through the 'stack' keyword and to its timestamps through the 'timestamps' keyword
 
 ```
 
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+from setuptools import setup, find_packages
+import itertools
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+
+# Parse the version from the geextract module.
+with open('geesarfetcher/__init__.py') as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip()
+            version = version.strip('"')
+            version = version.strip("'")
+            continue
+
+# 
+extra_reqs = {'docs': ['sphinx',
+                       'sphinx-rtd-theme',
+                       'sphinxcontrib-programoutput']}
+extra_reqs['all'] = list(set(itertools.chain(*extra_reqs.values())))
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
+print(readme)
+setup(name='geesarfetcher',
+      version=version,
+      description=u"Extract Sentinel-1 GRD time-series images over a given area from google earth engine",
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      classifiers=[],
+      keywords='SAR, sentinel, google, gee, time-series, radar, satellite imagery',
+      author=u"Thomas Di Martino",
+      author_email='thomas.di-martino@hotmail.com',
+      url='https://github.com/dimartinot/gee_sar_fetcher',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'tqdm>=4.50.2',
+          'numpy>=1.18.0,<=1.19.3',
+          'earthengine-api>=0.1.231'],
+      #scripts=['geextract/scripts/gee_extract.py',
+      #         'geextract/scripts/gee_extract_batch.py'],
+      test_suite="tests",
+      extras_require=extra_reqs)
